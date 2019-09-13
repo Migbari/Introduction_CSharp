856080180fe839cda5dd1a6612725dbb7d2631c0
FIC <FIC@info.local>
2019-09-13 09:35:37 -0300

    Arquivos Incluidos

---
+++ aula1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           //Somente declaramos, sem atribuir valor
+          
+            
+            //Declaramos atribuindo um valor
+            int num2 = 5;
+
+            //Declaramos diversas variáveis de mesmo tipo
+            int num5 = 7;
+
+            Console.WriteLine(num2 + num5);
+
+        }
+    }
+}
