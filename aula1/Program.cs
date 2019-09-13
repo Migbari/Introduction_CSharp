0b620d8c7b299185d7523cd49383c31e11c10bcb
FIC <FIC@info.local>
2019-09-13 10:55:20 -0300

    ReadLine  e WriteLine

--- aula1/Program.cs
+++ aula1/Program.cs
@@ -8,16 +8,45 @@ namespace aula1
         {
            
            //Somente declaramos, sem atribuir valor
-          
-            
+            int num1;
             //Declaramos atribuindo um valor
             int num2 = 5;
 
             //Declaramos diversas variáveis de mesmo tipo
-            int num5 = 7;
+            int num3, num4, num5;
 
-            Console.WriteLine(num2 + num5);
+            // Variável do tipo decimal (real)
+            float media;
 
+            // Variável do tipo lógico (booleana)
+            bool resultado = false;
+
+            string nomeAluno;
+            
+            // Quebra linha
+            Console.WriteLine();
+
+            // Escreve em uma linha quebrando-a
+            Console.WriteLine("Bem vindos a nossa primeira aplicação");
+
+            // Quebra linha
+            Console.Write("Digite seu nome: ");
+
+            // Continua escrevendo na mesma linha
+            Console.Write("Digite");
+            Console.Write(" Seu nome");
+
+            // Capturo o dado difitado pelo usuário e salvo na variável
+            nomeAluno = Console.ReadLine();
+
+            // Concatenação 
+            Console.WriteLine(nomeAluno + ", Agora digite sua primeira nota: ");
+            
+            // Converto o texto capturado para o tipo inteiro
+            num3 = int.Parse(Console.ReadLine());
+
+            // Interpolação/ Unir variável com txt
+            Console.WriteLine($"Sua primeira nota é {num3} ");
         }
     }
 }
