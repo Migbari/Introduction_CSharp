be901edf48f55316ed44ea1c7889f5d8d4b839a8
FIC <FIC@info.local>
2019-09-16 15:46:58 -0300

    inclusão de exercícios

---
+++ lista_exercicio_2.0/exercicio5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.WriteLine("Digite o primeiro número");
+            num1 = int.Parse(Console.ReadLine() );
+
+            Console.WriteLine("Digite o segundo número");
+            num2 = int.Parse(Console.ReadLine() );
+
+            Console.WriteLine("Digite o terceiro número");
+            num3 = int.Parse(Console.ReadLine() );
+
+            if(num1>num2 && num1>num3){
+                Console.WriteLine("Numero 1 maior");
+            }
+            else if(num2>num1 && num2>num3){
+                Console.WriteLine("Numero 2 maior");
+            }
+            else{
+                Console.WriteLine("Numero 3 maior");
+            }
+        }
+    }
+}
