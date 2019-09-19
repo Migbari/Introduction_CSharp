a60553e65a6005265dbd4edf1abada2aa748d073
FIC <FIC@info.local>
2019-09-19 16:07:19 -0300

    alterações

---
+++ lista_exercicio_3.0/exercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercicio1 {
+    class Program {
+        static void Main (string[] args) {
+
+
+            for (int cont = 1; cont <= 100; cont++) {
+               
+                //EXPRESSÃO CONTADOR AO CUBO (3) 
+                    Console.WriteLine (Math.Pow(cont,3) );
+            }
+
+
+        }
+    }
+}
